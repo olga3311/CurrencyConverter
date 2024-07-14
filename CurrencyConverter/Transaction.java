b006d387addae7862223264c4065eaa02a042d7c
wadim-81 <v1281@mail.ru>
2024-07-14 20:43:40 +0200

    comments second

--- CurrencyConverter/Transaction.java
+++ CurrencyConverter/Transaction.java
@@ -26,24 +26,25 @@ public class Transaction {
     @Override
     public String toString() {
         return "Transaction :" +
-                "timeStamp " + timeStamp + '\'' +
+                " " + timeStamp + '\'' +
                 "  amount " + amount +
                 "  from " + fromCurrency + '\'' +
-                "for " + convertedAmount +
+                "to " + convertedAmount +
                 "  " + toCurrency + '\'' +
                 '}';
 /**
- * Переопределение метода toString() для класса Transaction.
-                * @return    строковое представление объекта Transaction.
+ * в этом методе происходит Переопределение метода toString() для класса Transaction.
 
-      *  @Override
-      *  public String toString() {
-            return "Transaction{" +
-                    "timeStamp='" + timeStamp + '\'' +   Временная метка транзакции
-                    ", amount=" + amount +               Сумма транзакции
-                    ", from'" + fromCurrency + '\'' +    Исходная валюта
+ *
+
+ *  @Override
+ *  public String toString() {
+            return "Transaction" +
+                    "timeStamp='" + timeStamp + '\'' +         Временная метка транзакции
+                    ", amount=" + amount +                     Сумма транзакции
+                    ", from'" + fromCurrency + '\'' +          Исходная валюта
                     ", convertedAmount=" + convertedAmount +   Сумма после конвертации
-                    ", to'" + toCurrency + '\'' +        Целевая валюта
+                    ", to'" + toCurrency + '\'' +              Целевая валюта
                     '}';*/
 
     }
