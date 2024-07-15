53f5c616a1b52d0a2bca519cb5c8b932d4e27acc
wadim-81 <v1281@mail.ru>
2024-07-15 20:25:30 +0200

    comments second2

--- CurrencyConverter/TransactionHistory.java
+++ CurrencyConverter/TransactionHistory.java
@@ -2,20 +2,34 @@ package CurrencyConverter;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Класс TransactionHistory сохраняет все транзакции.
+ */
 public class TransactionHistory {
 
     private List<Transaction> transactions;
-
+    /**
+     * Конструктор инициализирует пустой список транзакций.
+     */
     public TransactionHistory() {
         transactions = new ArrayList<>();
-    }
 
+    }
+    /**
+     * Метод добавляет новую транзакцию в список.
+     *
+     * @param amount сумма в исходной валюте
+     * @param fromCurrency исходная валюта
+     * @param convertedAmount сумма в целевой валюте
+     * @param toCurrency целевая валюта
+     */
     public void addTransaction(double amount, String fromCurrency, double convertedAmount, String toCurrency) {
         transactions.add(new Transaction(amount, fromCurrency, convertedAmount, toCurrency));
 
     }
-
+    /**
+     * Метод выводит все транзакции в консоль.
+     */
     public void printTransactions() {
         for (int i = 0; i < transactions.size(); i++) {
             String transaction = String.valueOf(transactions.get(i));
